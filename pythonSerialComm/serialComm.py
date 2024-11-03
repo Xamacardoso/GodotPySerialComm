0180a7d049e1e977d679f5add1052c19234918e2
Xamacardoso <contato.xcardoso@gmail.com>
2024-11-03 13:25:38 -0300

    feats: added threadding and reconnection features

--- pythonSerialComm/serialComm.py
+++ pythonSerialComm/serialComm.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import socket
+import threading
 
 # Bilblioteca para acessar as portas seriais do arduino.
 import serial
@@ -11,22 +12,37 @@ def readData(baudrate, udp_ip, udp_port):
     '''Lê a primeira porta serial que está tentando enviar informações
     e envia as informações para a Godot via UDP.'''
 
-    try:
-        ports = serial.tools.list_ports.comports(); # Pega a lista de portas que estão usando conexão serial
-        port = ports[0].name;  # Tenta conectar com a primeira porta serial
-        ser = serial.Serial(port, baudrate, timeout=1)  # Tenta conectar com a porta serial do Arduino
-        
-        # Mostra as portas seriais disponíveis
-        print("Portas: ")
-        for port in ports:
-            print(port.name)
-
-
-        # Configura o socket UDP para mandar o pacote
-        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_adress = (udp_ip, udp_port)
-
+    def connectSerial():
+        '''Tenta conectar na primeira porta serial que está enviando informações'''
         while True:
+            ports = serial.tools.list_ports.comports(); # Pega a lista de portas que estão usando conexão serial
+            for port in ports:
+                try:
+                    ser = serial.Serial(port.device, baudrate, timeout=1)  # Tenta conectar com a porta serial do Arduino
+                    print(f"Conectado à porta serial: {port.device}")
+                    return ser
+
+                except (serial.SerialException, IndexError) as e:
+                    print("Erro de comunicação serial: ", e);
+
+            print("Sem portas seriais disponíveis para conexão")
+            return None
+
+    ser = None
+    
+    # Configura o socket UDP para mandar o pacote
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_adress = (udp_ip, udp_port)
+    while True:
+
+        try:
+            if ser is None:
+                ser = connectSerial()
+                if ser is None:
+                    print("Reconectando em 3 segundos...")
+                    time.sleep(3)
+                    continue
+
             if ser.in_waiting > 0:  # Se há dados esperando para serem lidos do Arduino
                 
                 # Decodifica e remove caracteres indesejados
@@ -34,32 +50,29 @@ def readData(baudrate, udp_ip, udp_port):
                 # Remove caracteres adicionais, se presentes
                 clean_line = line.replace("\r", "").replace("\n", "")  
 
-
                 print(clean_line)  # Imprime a linha limpa no console para verificação
                 try:
                     udp_socket.sendto(clean_line.encode(), udp_adress) # Envia um pacote via UDP que contém a linha
                     print("Pacote enviado.")
                 except Exception as e:
-                    print(f"Erro ao enviar pacote: {e}")
-                    break
-
+                    print(f"Erro ao enviar pacote: {e}")            
             time.sleep(0.1)  # Delay de leitura; ajuste conforme necessário
 
-    except serial.SerialException as e:
-        print(f"Erro de comunicação serial: {e}")
-
-    except IndexError as e:
-        print(f"Erro, não há conexões seriais disponíveis!")
-
-    except Exception as e:
-        print(f"Erro de conexão UDP: {e}")
+        except (serial.SerialException, Exception) as e:
+            print(f"Erro de leitura/envio de dados: {e}")
+            ser.close()
+            ser = None
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 3:
         baudrate = int(sys.argv[1])
         ip = sys.argv[2]
         port = int(sys.argv[3])
-        readData(baudrate, ip, port)
+        serial_thread = threading.Thread(target=readData, args=(baudrate, ip, port))
+        serial_thread.start()
+        serial_thread.join()
+
     else:
         print("Uso: python serial_comm.py <baudrate> <ip> <port>")
+    
